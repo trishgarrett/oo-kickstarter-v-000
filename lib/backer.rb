0c2ff5326a22e186905fddcbcff5a4ee5cc8b617
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-01 02:26:13 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,12 @@
 require 'pry'
 
 class Backer
-  attr_accessor :name, :backed_projects
+  attr_accessor :name
+  attr_writer :backed_projects
+
+  def backed_projects
+    @backed_projects
+  end  
 
   def initialize(name)
     @name = name
